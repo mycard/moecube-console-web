86a0c1dc00756f91b58f61bba53ddc9f9ae0159a
nano <ncysatnaf@gmail.com>
2017-04-24 12:02:48 +0800

    test params

--- .roadhogrc.js
+++ .roadhogrc.js
@@ -1,22 +1,39 @@
 let publicPath = 'https://cdn01.moecube.com/console/';
 
+const API_ROOT = {
+  development: 'http://localhost:8001',
+  test: 'http://114.215.243.95:8001',
+  production: 'https://moecube.com/console'
+};
+const RETURN_SSO = {
+  development: `http://localhost:8000/loginCallback`,
+  test: `http://114.215.243.95:8000/loginCallback`,
+  production: `https://moecube.com/console/loginCallback`
+};
+
+let defineConf = {
+  apiRoot: API_ROOT[process.env['ENV']],
+  returnSSO: RETURN_SSO[process.env['ENV']]
+};
+
 export default {
   publicPath,
   "entry": "src/index.js",
+  define: defineConf,
   "env": {
     "development": {
       "extraBabelPlugins": [
         "dva-hmr",
         "transform-runtime",
         "babel-plugin-transform-decorators-legacy",
-        ["import", { "libraryName": "antd", "style": "css" }]
+        ["import", {"libraryName": "antd", "style": "css"}]
       ]
     },
     "production": {
       "extraBabelPlugins": [
         "transform-runtime",
         "babel-plugin-transform-decorators-legacy",
-        ["import", { "libraryName": "antd", "style": "css" }]
+        ["import", {"libraryName": "antd", "style": "css"}]
       ]
     }
   }
--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 export default {
-  apiRoot: 'http://localhost:8001',
-  imgRoot: 'http://node:7888',
-  returnSSO: 'http://localhost:8000/loginCallback',
+  apiRoot,
+  returnSSO,
   SSOProvider: 'https://accounts.moecube.com/'
-  // SSOProvider: 'http://localhost:8081'
 }
